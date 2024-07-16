f9338e64feaa21f571abce0ebb7d5dda21dbcb0c
Tiana Coker <tianacoker1@gmail.com>
2024-07-16 20:05:51 +0100

    Introduction to components

---
+++ src/Food.jsx
@@ -0,0 +1,17 @@
+
+function Food(){
+  
+  const food1 = "Jollof Rice";
+  const food2 = "Asun Rice";
+
+
+ return(
+
+   <ul>
+    <li>Apple</li>
+    <li>{food1}</li>
+    <li>{food2.toUpperCase()}</li>
+   </ul>
+ );
+}
+export default Food
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+
+  return(
+    <footer>
+      <p>&copy;{new Date().getFullYear()} Deliciouz</p>
+    </footer>
+  );
+}
+
+export default Footer
